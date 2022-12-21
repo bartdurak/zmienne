f9c8d81434ce1d7b1d1605ef43ecdbeee17d2d61
bartdurak <projektator@gmail.com>
2022-12-21 01:52:47 +0100

    zmienne

---
+++ zmienne.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+unsigned short int Width, Length;
+Width = 5;
+Length= 10;
+unsigned short int Area;
+Area = Width * Length;
+
+cout << Width << " szerokość , długośc " << Length << endl;
+cout<< "obszar " << Area <<" cm" << endl;
+
+    return 0;
+}
